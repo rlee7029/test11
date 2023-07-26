234b0d87298bdd5ec12c305db72ac3cf091158c9
Robert Lee <robertlee@roberts-mbp.lan>
2023-07-26 11:28:29 -0700

    code

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import App from './App';
 
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_API_URI, //'http://localhost:3000/graphql', 
+  uri: /graphql, //'http://localhost:3000/graphql', 
   cache: new InMemoryCache(),
 });
 
