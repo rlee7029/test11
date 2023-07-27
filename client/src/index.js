0f20df92fbc1687d0f1053c9074c45c8b3068906
Robert Lee <robertlee@Roberts-MacBook-Pro.local>
2023-07-26 20:04:26 -0700

    code

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import App from './App';
 
 const client = new ApolloClient({
-  uri: /graphql, //'http://localhost:3000/graphql', 
+  uri: '/graphql', //'http://localhost:3000/graphql', 
   cache: new InMemoryCache(),
 });
 
